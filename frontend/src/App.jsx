bf6ca52d3dbc50b05f5658e202a5bc1a127171ba
devilAtlastBreath <omkarp.0906@gmail.com>
2026-02-07 19:55:13 +0530

    backend base

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import Sender from "./pages/Features/Sender";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
-    <div className="main">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    <div>
+      <Sender/>
     </div>
   );
 }
---
+++ frontend/src/pages/Features/Sender.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Sender() {
+  return (
+    <div>
+        <div>
+            <p>Whatsapp Number: </p>
+            <input type="text" />
+            <p>Email Address: </p>
+            <input type="text" />
+        </div>
+    </div>
+  )
+}
