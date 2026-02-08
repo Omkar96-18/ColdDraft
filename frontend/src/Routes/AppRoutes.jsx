818c0e76af8eeb5c0dc3b1576f475409049b3811
devilAtlastBreath <omkarp.0906@gmail.com>
2026-02-08 15:30:20 +0530

    frontend checked

--- frontend/src/Routes/AppRoutes.jsx
+++ frontend/src/Routes/AppRoutes.jsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { Routes, Route, Navigate } from "react-router-dom";
-import Login from "../Pages/Login";
-import Register from "../Pages/Register";
-import Home from "../Pages/Home";
 
-const AppRoutes = () => {
+import { Routes, Route, Navigate } from "react-router-dom";
+import Login from "../pages/registration/Login";
+import Register from "../pages/registration/Register";
+import Home from "../pages/main/Home";
+
+function AppRoutes ()  {
   return (
     <Routes>
-      {/* Auth Routes */}
+ 
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/home" element={<Home/>} />
 
-      {/* Default Redirect: Send users to login if path is unknown */}
       <Route path="*" element={<Navigate to="/login" replace />} />
 
       
